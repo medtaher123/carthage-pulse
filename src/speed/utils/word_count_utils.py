e3711795dd9ab148c060e394a4cfc50cb841f371
medtaher123 <medtaherbh@gmail.com>
2026-05-07 23:45:30 +0100

    feat: add stop word

--- src/speed/utils/word_count_utils.py
+++ src/speed/utils/word_count_utils.py
@@ -6,7 +6,7 @@ from pyspark.sql import DataFrame
 STOP_WORDS = [
     "the", "and", "this", "that", "with", "from", "for",
     "what", "your", "would", "want", "have", "when", "they", "there", "which", "about", "like", "just", "more",
-    "here", "actually",
+    "here", "actually", "https",
     "eli", "mta3", "ala", "fama", "haka", "bech", "fi", # Derja/Arabic
     "ken", "houwa", "hiyya", "mouch", "bara", "aya"
 ]
